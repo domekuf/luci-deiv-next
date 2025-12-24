cb99fb076f1edd0597c13cf0c5b75a7c1bc4911c
Domenico Di Iorio <dome.diiorio@gmail.com>
2025-12-24 20:34:56 +0100

    get by slug

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -1,16 +1,19 @@
-import { getSheet } from "@/lib/getSheet";
+import { getBySlug } from "@/lib/getBySlug";
 
-export default async function SlugPage({ params }: { params: Promise<{ slug: string }> }) {
-  const sheet = await getSheet();
+export default async function SlugPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const slug = (await params).slug;
+  const entries = await getBySlug(slug);
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-center py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-          Slug: {(await params).slug}
+          Slug: {slug}
         </h1>
-        <p>
-          Sheet Data: {JSON.stringify(sheet)}
-        </p>
+        <p>Sheet Data: {JSON.stringify(entries)}</p>
       </main>
     </div>
   );
---
+++ lib/getBySlug.ts
@@ -0,0 +1,28 @@
+import { getSheet } from "./getSheet";
+
+export async function getBySlug(slug: string) {
+  const filtered = [];
+
+  const rows = await getSheet();
+  if (!rows) {
+    return [];
+  }
+  for (let i = 0; i < rows?.length; ++i) {
+    const row = rows[i];
+    if (row[0] === slug) {
+      filtered.push({
+        id: slug,
+        name: row[1],
+        range: `D${i + 1}`,
+        line: `${i + 1}`,
+        checked: row[3] == 1,
+        under5: row[5] == 1,
+        bet5and10: row[6] == 1,
+        gluten: row[7] == 1,
+        lactose: row[8] == 1,
+        notes: row[9],
+      });
+    }
+  }
+  return filtered;
+}
